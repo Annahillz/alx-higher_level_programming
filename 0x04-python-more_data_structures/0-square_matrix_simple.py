cc039a7f2c90982f09f00c90c7ae3972c49f81e0
Anabel Emebo <emeboanabel@gmail.com>
2022-06-08 13:36:58 -0700

    squared matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for items in matrix:
-       new_matrix.append([x ** 2 for x in items])
+        new_matrix.append([x ** 2 for x in items])
     return new_matrix
