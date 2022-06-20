d12f988029a886a58c2f411d273ef9e08bd4d685
Anabel Emebo <emeboanabel@gmail.com>
2022-06-20 09:07:48 -0700

    safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    real_num = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            real_num += 1
+        except (ValueError, IndexError, TypeError):
+            print("", end="")
+    print()
+    return(real_num)
+
