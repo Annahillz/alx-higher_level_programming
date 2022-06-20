06d3cc1ebde1b302d87eb69c80ee28dd2a817f3c
Anabel Emebo <emeboanabel@gmail.com>
2022-06-20 09:10:43 -0700

    safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
             real_num += 1
         except (ValueError, IndexError, TypeError):
-            print("", end="")
+            break
     print()
     return(real_num)
 
