8c5e274bdaf5a3a65977d45cd5a2f46154a1a700
Anabel Emebo <emeboanabel@gmail.com>
2022-07-04 16:11:22 -0700

    Inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -9,6 +9,7 @@ class MyInt(int):
     def __new__(cls, *args, **kwargs):
         """create a new instance of the class"""
         return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
     def __eq__(self, other):
         """what was != is now =="""
         return int(self) != other
