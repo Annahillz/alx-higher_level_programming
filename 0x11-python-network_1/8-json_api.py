15a4f7374b6d228978b943a6b3e68362f33a2b39
Anabel Emebo <emeboanabel@gmail.com>
2022-09-23 16:00:12 -0700

    network

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python3i
-""" send POST request """
-import requests
+#!/usr/bin/python3
+"""Sends a search parameter to a URL."""
 import sys
+import requests
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sr = {"q": ""}
-    else:
-        sr = {"q": sys.argv[1]}
-    response = requests.post("http://0.0.0.0:5000/search_user", data=sr)
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    query = sys.argv[1] if len(sys.argv) > 1 else ""
+    # if len(query) > 0 and not query[0].isalpha():
+    #     query = ""
+    form_data = [('q', query)]
+    response = requests.post(url, data=form_data)
     try:
-        result = response.json()
-        if result == {}:
-            print("No result")
+        json_content = response.json()
+        if json_content:
+            print('[{}] {}'.format(json_content['id'], json_content['name']))
         else:
-            print("[{}] {}".format(result.get("id"), result.get("name")))
-    except ValueError:
-        print("Not a valid JSON")
+            print('No result')
+    except Exception:
+        print('Not a valid JSON')
