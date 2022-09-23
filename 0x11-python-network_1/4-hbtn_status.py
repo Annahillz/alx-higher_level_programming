8ebc05cb2ac64571a4e5755ef41f4518e6fa95e5
Anabel Emebo <emeboanabel@gmail.com>
2022-09-23 16:09:58 -0700

    network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetches a URL."""
-import requests
+""" requests model """
 
 
 if __name__ == '__main__':
-    response = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+    import requests
+    html = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
