14867dde08bc1a5c4721764c797ccd3aa2f1843a
Anabel Emebo <emeboanabel@gmail.com>
2022-10-18 15:20:31 -0700

    js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-const content = process.argv[3];
-fs.writeFile(file, content, 'utf-8', function (err) {
-  if (err) {
-    console.log(err);
-  }
-});
+
+if (process.argv.length > 2) {
+  fs.readFile(process.argv[2], (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data.toString('utf-8'));
+    }
+  });
+}
